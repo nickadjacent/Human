210c2a41da64b9da94afe318e0a6148d1227b565
Nick_Ram <60027609+nickadjacent@users.noreply.github.com>
2020-04-09 00:56:58 -0700

    Human

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Human
+{
+    // class Program
+    // {
+    //     static void Main(string[] args)
+    //     {
+    //         Console.WriteLine();
+    //     }
+    // }
+
+    class Human
+    {
+        // Fields for Human
+        public string Name;
+        public string Strength;
+        public string Intelligence;
+        public string Dexterity;
+        private string Health;
+
+
+        // add a public "getter" property to acces health
+
+
+        // Add a constructor that takes a value to set Name, and set the remaining fields to defualt values
+
+
+        // Add a constructor to assign custom values to all fields
+
+
+        // Build an Attack method
+        public int Attack(Human target)
+        {
+            Console.WriteLine();
+        }
+    }
+}
